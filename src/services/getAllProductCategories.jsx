4c6a326a4bd0a7118cbe6f330db192667ca59ed3
IchaKhairunisa <khairunisalsabilakurniawan@gmail.com>
2024-11-18 03:43:23 +0700

    17 Hooks

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,26 @@
+const getAllProductCategories = ()  => {
+    return [   
+      {
+        id: 'E',
+        slug: 'espresso',
+        name: `Espresso`
+      },
+      {
+        id: 'BF',
+        slug: 'brewed-coffee',
+        name: `Brewed Coffee`
+      },
+      {
+        id: 'NC',
+        slug: 'non-coffee',
+        name: `Non Coffee`
+      },
+      {
+        id: 'F',
+        slug: 'food',
+        name: `Food`
+      },
+    ]
+  }
+  
+export default getAllProductCategories
